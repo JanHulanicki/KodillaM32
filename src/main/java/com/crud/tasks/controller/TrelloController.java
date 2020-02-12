5c625630b530471c0f3b4631d9e5b9c91179ef8a
Jan Hulanicki <jan_hulanicki@poczta.onet.pl>
2020-02-12 21:40:19 +0100

    M23.1 init

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,4 +1,5 @@
 package com.crud.tasks.controller;
+
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.trello.client.CreatedTrelloCard;
@@ -10,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URISyntaxException;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/v1/trello")
 public class TrelloController {
@@ -19,27 +23,27 @@ public class TrelloController {
     @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
     public List<TrelloBoardDto> getTrelloBoards() {
         // GET request
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+     //   List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
-        trelloBoards.forEach(trelloBoardDto -> {
+     //   trelloBoards.forEach(trelloBoardDto -> {
 
-            System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
+      ///      System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
 
-            System.out.println("This board contains lists: ");
+      //      System.out.println("This board contains lists: ");
 
-            trelloBoardDto.getLists().forEach(trelloList ->
-                    System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
+       //     trelloBoardDto.getLists().forEach(trelloList ->
+       //             System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
 
-        });
+      //  });
 
-        // List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-        //  Optional.ofNullable(trelloBoards)
-        //          .orElse(Collections.emptyList())
-        //         .forEach(trelloBoardDto -> {
-        //             if((trelloBoardDto.getName().contains("Kodilla"))) {
-        //                 System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
-        //             }
-        //         });
+         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+          Optional.ofNullable(trelloBoards)
+                  .orElse(Collections.emptyList())
+                 .forEach(trelloBoardDto -> {
+                     if((trelloBoardDto.getName().contains("Kodilla"))) {
+                         System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+                     }
+                 });
             //zadanie podpunkt 3
             //trelloBoards.forEach(trelloBoardDto -> {
             //  if((trelloBoardDto.getId()!=null)&&
