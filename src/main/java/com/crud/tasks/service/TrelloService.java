bd79625b61a2030e5e47c77affe7b4e467dc161f
JanH <jan_hulanicki@poczta.onet.pl>
2020-02-14 14:14:15 +0100

    M23.4 cwiczenie

---
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -0,0 +1,34 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.trello.client.CreatedTrelloCard;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.net.URISyntaxException;
+import java.util.List;
+
+@Service
+public class TrelloService {
+    private static final String SUBJECT = "Tasks: New Trello card";
+    @Autowired
+    private TrelloClient trelloClient;
+    @Autowired
+    private SimpleEmailService emailService;
+    public List<TrelloBoardDto> fetchTrelloBoards() {
+        return trelloClient.getTrelloBoards();
+    }
+    public CreatedTrelloCard createTrelloCard(final TrelloCardDto trelloCardDto) throws URISyntaxException {
+        CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
+        emailService.send(new Mail(
+                "",
+                "",
+                SUBJECT,
+                "New card: "+ trelloCardDto.getName() + "has been created on your Trello account"));
+        return newCard;
+    }
+
+}
