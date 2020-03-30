843d933f2269f4a00ea88fee51ea90ec3dab4ee5
JanH <jan_hulanicki@poczta.onet.pl>
2020-03-30 14:17:28 +0200

    M29.3 zadanie TestTaskController

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,4 +1,5 @@
 package com.crud.tasks.controller;
+
 import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
@@ -17,32 +18,33 @@ public class TaskController {
     private DbService service;
     @Autowired
     private TaskMapper taskMapper;
+
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
         return taskMapper.mapToTaskDtoList(service.getAllTasks());
     }
+
     @RequestMapping(method = RequestMethod.GET, value = "getTaskById")
-        public TaskDto getTaskById(@RequestParam Long taskId) throws TaskNotFoundException
-        {
+    public TaskDto getTaskById(@RequestParam Long taskId) throws TaskNotFoundException {
         return taskMapper.mapToTaskDto(service.getTaskById(taskId).orElseThrow(TaskNotFoundException::new));
     }
-    @RequestMapping(method = RequestMethod.PUT, value = "updateTaskNew")
-    public TaskDto  updateTaskNew(@RequestBody TaskDto taskDto) {
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
+    public TaskDto updateTaskNew(@RequestBody TaskDto taskDto) {
         return taskMapper.mapToTaskDto(service.saveTask(taskMapper.mapToTask(taskDto)));
     }
+
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
-    public TaskDto  getTask(Long taskId){
-        return new TaskDto(1L,"test title","test_content");
+    public TaskDto getTask(Long taskId) {
+        return new TaskDto(1L, "test title", "test_content");
     }
+
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
-    public void 	deleteById(@RequestParam Long taskId){
-       service.deleteById(taskId);
+    public void deleteById(@RequestParam Long taskId) {
+        service.deleteById(taskId);
 
     }
-    @RequestMapping(method = RequestMethod.GET, value = "updateTask")
-    public TaskDto updateTask(TaskDto taskDto){
-        return new TaskDto(1L,"Edited test title","Test content");
-    }
+
     @RequestMapping(method = RequestMethod.POST, value = "createTask", consumes = APPLICATION_JSON_VALUE)
     public void createTask(@RequestBody TaskDto taskDto) {
         service.saveTask(taskMapper.mapToTask(taskDto));
---
+++ src/test/java/com/crud/tasks/controller/TaskControllerTest.java
@@ -0,0 +1,131 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(TaskController.class)
+class TaskControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private TaskMapper taskMapper;
+    @MockBean
+    private DbService service;
+
+    @Test
+    void TestGetEmptyTask() throws Exception {
+        //Given
+        List<TaskDto> taskList = new ArrayList<>();
+        when(taskMapper.mapToTaskDtoList(service.getAllTasks())).thenReturn(taskList);
+        //When @ Then
+        mockMvc.perform(get("/v1/task/getTasks").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                //Trello boards fields
+                .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    void TestGetTasks() throws Exception {
+        //Given
+        List<TaskDto> taskList = new ArrayList<>();
+        taskList.add(new TaskDto(1L, "title1", "content1"));
+        when(taskMapper.mapToTaskDtoList(service.getAllTasks())).thenReturn(taskList);
+        //When & Then
+        mockMvc.perform(get("/v1/task/getTasks").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].title", is("title1")))
+                .andExpect(jsonPath("$[0].content", is("content1")));
+    }
+
+    @Test
+    void getTaskById() throws Exception {
+        //Given
+        TaskDto taskDto = new TaskDto(1L, "title1", "content1");
+        Task task = new Task(1L, "title1", "content1");
+        when(service.getTaskById(1L)).thenReturn(Optional.ofNullable(task));
+        when(taskMapper.mapToTaskDto(task)).thenReturn(taskDto);
+        //When & Then
+        mockMvc.perform(get("/v1/task/getTaskById?taskId=1").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.title", is("title1")))
+                .andExpect(jsonPath("$.content", is("content1")));
+    }
+
+    @Test
+    void testCreateTask() throws Exception {
+        //Given
+        TaskDto taskDto = new TaskDto(1L, "title1", "content1");
+        Task task = new Task(1L, "title1", "content1");
+        when(taskMapper.mapToTask(ArgumentMatchers.any())).thenReturn(task);
+        when(service.saveTask(task)).thenReturn(ArgumentMatchers.any());
+        when(taskMapper.mapToTaskDto(task)).thenReturn(taskDto);
+        Gson gson = new Gson();
+        String jsonContent = gson.toJson(taskDto);
+        //When & Then
+        mockMvc.perform(post("/v1/task/createTask").contentType(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content(jsonContent))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testUpdateTask() throws Exception {
+        //Given
+        TaskDto taskDto = new TaskDto(1L, "title1", "content1");
+        Task task = new Task(1L, "title1", "content1");
+        when(taskMapper.mapToTask(ArgumentMatchers.any())).thenReturn(task);
+        when(service.saveTask(task)).thenReturn(ArgumentMatchers.any());
+        when(taskMapper.mapToTaskDto(task)).thenReturn(taskDto);
+        Gson gson = new Gson();
+        String jsonContent = gson.toJson(taskDto);
+        //When & Then
+        mockMvc.perform(put("/v1/task/updateTask").contentType(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content(jsonContent))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.title", is("title1")))
+                .andExpect(jsonPath("$.content", is("content1")));
+    }
+
+    @Test
+    void deleteById() throws Exception {
+        //Given
+        TaskDto taskDto = new TaskDto(1L, "title1", "content1");
+        Task task = new Task(1L, "title1", "content1");
+        when(service.getTaskById(1L)).thenReturn(Optional.ofNullable(task));
+        when(taskMapper.mapToTaskDto(task)).thenReturn(taskDto);
+        //When & Then
+        mockMvc.perform(delete("/v1/task/deleteTask?taskId=1").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
